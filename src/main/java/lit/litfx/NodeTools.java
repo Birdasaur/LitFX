0bd9b14a904596936a79eba8570b4aa68e0a7b87
Sean Phillips <sean.mi.phillips@gmail.com>
2019-08-05 11:45:56 -0400

    new LitGuiDemo demonstrates arc lightning between arbitrary node objects

--- src/main/java/lit/litfx/NodeTools.java
+++ src/main/java/lit/litfx/NodeTools.java
@@ -1,6 +1,7 @@
 package lit.litfx;
 
 import java.util.ArrayList;
+import java.util.Properties;
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
@@ -16,17 +17,17 @@ public enum NodeTools {
     INSTANCE;
     public static Bolt litBounds(Node node1, Node node2) {
         Bounds bounds = node1.getBoundsInLocal();
-        Bounds screenBounds = node1.localToScreen(bounds);        
-        double minXNode1 = screenBounds.getMinX();
-        double minYNode1 = screenBounds.getMinY();
+        Bounds parentBounds = node1.localToParent(bounds);        
+        double minXNode1 = parentBounds.getMinX();
+        double minYNode1 = parentBounds.getMinY();
         bounds = node2.getBoundsInLocal();
-        screenBounds = node2.localToScreen(bounds);        
-        double minXNode2 = screenBounds.getMinX();
-        double minYNode2 = screenBounds.getMinY();
+        parentBounds = node2.localToParent(bounds);        
+        double minXNode2 = parentBounds.getMinX();
+        double minYNode2 = parentBounds.getMinY();
         System.out.println(minXNode1 + ", " + minYNode1 + " -> " + minXNode2 + ", " + minYNode2);
         Bolt bolt = new Bolt(
             new Point2D(minXNode1, minYNode1), new Point2D(minXNode2, minYNode2),
-            0.05, 80, 10, 0.75, 0.25);   
+            0.1, 80, 15, 0.85, 0.1); 
         bolt.setStroke(Color.ALICEBLUE);
         return bolt;
     }
--- src/main/java/lit/litfx/demos/LitGuiDemo.java
+++ src/main/java/lit/litfx/demos/LitGuiDemo.java
@@ -1,14 +1,24 @@
 package lit.litfx.demos;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Bloom;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.SepiaTone;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import lit.litfx.NodeTools;
@@ -22,25 +32,32 @@ public class LitGuiDemo extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        StackPane litPane = new StackPane();
+        Pane litPane = new Pane();
         litPane.setMouseTransparent(true);
-        
         Button button1 = new Button("Button 1");
         Button button2 = new Button("Button 2");
         button1.setOnAction(action -> {
             litPane.getChildren().removeIf(node -> node instanceof Bolt);
             Bolt bolt = NodeTools.litBounds(button1, button2);
-            if(null != bolt) {
-                litPane.getChildren().add(bolt);
-                bolt.setVisibleLength(0);
-                bolt.animate(Duration.millis(100));
-            }            
+            setBoltEffects(bolt);
+            litPane.getChildren().add(bolt);
+            //System.out.println("First Point: " + bolt.getPoints().get(0) + ", " + bolt.getPoints().get(1));
+            bolt.setVisibleLength(0);
+            bolt.animate(Duration.millis(100));
         });
         HBox hbox = new HBox(400, button1, button2);
         hbox.setAlignment(Pos.CENTER);
 
-        StackPane centerStack = new StackPane(hbox, litPane);
-
+        BorderPane litBP = new BorderPane(litPane);
+        litBP.setBackground(Background.EMPTY);
+        litBP.setMouseTransparent(true);
+        StackPane centerStack = new StackPane(hbox, litBP);
+//        centerStack.widthProperty().addListener(event -> {
+//            litPane.resize(centerStack.getWidth(), centerStack.getHeight());
+//        });
+//        centerStack.heightProperty().addListener(event -> {
+//            litPane.resize(centerStack.getWidth(), centerStack.getHeight());
+//        });
         
         BorderPane root = new BorderPane(centerStack);
         root.setBackground(Background.EMPTY);
@@ -49,6 +66,24 @@ public class LitGuiDemo extends Application {
         primaryStage.setTitle("Click to fire bolt of lightning.");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+    }
+    
+    private void setBoltEffects(Bolt bolt) {
+        bolt.setStroke(Color.ALICEBLUE);
+        bolt.setOpacity(0.75);
+        bolt.setStrokeWidth(4.0);
+
+
+        SepiaTone st = new SepiaTone(0.25);
+        Bloom bloom = new Bloom(0.25);
+        bloom.setInput(st);
+        Glow glow = new Glow(0.75);
+        glow.setInput(bloom);
+        DropShadow shadow = new DropShadow(BlurType.GAUSSIAN, Color.CORNSILK, 10, 0.5, 0, 0);
+        shadow.setInput(glow);
+        shadow.setRadius(60.0);
+        bolt.setEffect(shadow);
     }
     
     /**
