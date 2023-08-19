200486ea38828f436f6476673d19a06868bdb3a6
samypr100 <3933065+samypr100@users.noreply.github.com>
2023-08-19 12:12:39 -0400

    Initial CI Support ci: gha ci support feat!: remove maven-release-plugin and moved to more CI friendly approach feat: update module-info to express transitives between modules feat: added github packages deployment support feat: added maven flattening to prep for Maven 5 behavior breaking: moved ossrh deployment into dedicated profile breaking: made sure javafx dependencies are marked as runtime for consumer poms (prep for Maven 5 changes) chore: use maven properties for versions chore: re-enable jlink in litfx-demos chore: updated JavaFX dependencies to 20.0.2 style: add .editorconfig and .gitattributes

--- litfx-controls/src/main/java/module-info.java
+++ litfx-controls/src/main/java/module-info.java
@@ -2,7 +2,7 @@
  * @author Birdasaur
  */
 module lit.litfx.controls {
-    requires lit.litfx.core;
+    requires transitive lit.litfx.core;
     requires javafx.controls;
     exports lit.litfx.controls.menus;
     exports lit.litfx.controls.output;
--- litfx-demos/src/main/java/module-info.java
+++ litfx-demos/src/main/java/module-info.java
@@ -5,9 +5,8 @@
  */
 module lit.litfx.demos {
     requires javafx.controls;
-    requires javafx.fxml;    
-    requires lit.litfx.core;
-    requires lit.litfx.controls;
+    requires javafx.fxml;
+    requires transitive lit.litfx.controls;
     opens lit.litfx.demos to javafx.fxml;
     opens lit.litfx.demos.controllers to javafx.fxml;
     exports lit.litfx.demos to javafx.graphics;
